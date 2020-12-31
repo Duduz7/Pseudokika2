adbf8eb9b213b1cba8214285aca044c50109b002
Duduz7 <duduzzz289@gmail.com>
2020-12-31 16:13:35 -0400

    CSS responsiveness finished, starting to focus almost fully on typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Material Modules
 import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
@@ -39,6 +40,7 @@ import { ZeroComponent } from './zero/zero.component';
     MatIconModule,
     MatButtonModule,
     MatInputModule,
+    MatListModule,
     MatExpansionModule,
     MatFormFieldModule,
     MatStepperModule,
